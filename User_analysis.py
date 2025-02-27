61b26f9c36cde494de455cf15f6182451f71f398
AnestisDamianou <anestisdamianou@gmail.com>
2025-02-27 14:09:18 +0200

    I fine the length of eac list of spatial errors. I compute the cumsum excluding the last one from the length and then create a graph that plots the combined spatial errors that also shows the transition points form one trial to another

--- User_analysis.py
+++ User_analysis.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from Lib_grip import spatial_error
 
-directory = r'C:\Users\USER\Desktop\Biomechanincs\projects\Grip Strength\pilot trials\Pink_65.1\trials'
+directory = r'C:\Users\USER\OneDrive - Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης\Grip training\Pilot study 4\Data\White_65.1\Training_trials'
 os.chdir(directory)
 
 # data = pd.read_csv(, skiprows=2)
@@ -18,16 +18,16 @@ os.chdir(directory)
 # plt.show()
 
 
-df1 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_13_33.csv", skiprows=2)
-df2 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_16_00.csv", skiprows=2)
-df3 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_18_44.csv", skiprows=2)
-df4 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_21_08.csv", skiprows=2)
-df5 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_23_17.csv", skiprows=2)
-df6 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_25_53.csv", skiprows=2)
-df7 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_28_12.csv", skiprows=2)
-df8 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_30_30.csv", skiprows=2)
-df9 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_32_48.csv", skiprows=2)
-df10 = pd.read_csv(r"grip_Makri_Stephani__11Φεβ25_12_35_23.csv", skiprows=2)
+df1 = pd.read_csv(r"White_65.1_trial_1.csv", skiprows=2)
+df2 = pd.read_csv(r"White_65.1_trial_2.csv", skiprows=2)
+df3 = pd.read_csv(r"White_65.1_trial_3.csv", skiprows=2)
+df4 = pd.read_csv(r"White_65.1_trial_4.csv", skiprows=2)
+df5 = pd.read_csv(r"White_65.1_trial_5.csv", skiprows=2)
+df6 = pd.read_csv(r"White_65.1_trial_6.csv", skiprows=2)
+df7 = pd.read_csv(r"White_65.1_trial_7.csv", skiprows=2)
+df8 = pd.read_csv(r"White_65.1_trial_8.csv", skiprows=2)
+df9 = pd.read_csv(r"White_65.1_trial_9.csv", skiprows=2)
+df10 = pd.read_csv(r"White_65.1_trial_10.csv", skiprows=2)
 
 pd.set_option('display.max_rows', None)
 
@@ -54,22 +54,23 @@ spatial_errors9 = spatial_error(new_df9)
 spatial_errors10 = spatial_error(new_df10)
 
 
-
-
-#new_df1_anestis = lb.synchronization_of_Time_and_ClosestSampleTime_Anestis(df1)
-#new_df1_Stylianos = lb.synchronization_of_Time_and_ClosestSampleTime_Stylianos(df1, 100)
-#
-#new_df1_anestis.to_excel(r'Anestis_pert.xlsx')
-#new_df1_Stylianos.to_excel(r'Stylianos_pert.xlsx')
-#sd = 2
-#consecutive_values = 37
-#first_values = 100
-#total_targets = 500
-#
-#perturbation = lb.adaptation_time_using_sd(df1, sd, first_values, consecutive_values, 'Akis', plot=True)
-
-
 combined_spatial_errors = list(itertools.chain(spatial_errors1, spatial_errors2, spatial_errors3, spatial_errors4, spatial_errors5, spatial_errors6, spatial_errors7, spatial_errors8, spatial_errors9, spatial_errors10))
-cumsum_combined_spatial_errors = np.cumsum(combined_spatial_errors)
-plt.plot(cumsum_combined_spatial_errors)
+
+# Finding the length of each list of spatial errors
+spatial_error_lengths = [len(spatial_errors1), len(spatial_errors2), len(spatial_errors3), len(spatial_errors4), len(spatial_errors5),len(spatial_errors6), len(spatial_errors7), len(spatial_errors8), len(spatial_errors9), len(spatial_errors10)]
+print(spatial_error_lengths)
+
+# Exclude last cumulative sum as it's the total length
+spatial_error_indices = np.cumsum(spatial_error_lengths)[:-1]
+
+# Plot the combined spatial errors
+plt.plot(combined_spatial_errors, label='Spatial Errors', linewidth=1)
+# Add red vertical lines at the transition points
+for idx in spatial_error_indices:
+    plt.axvline(x=idx, color='black', linestyle='--', linewidth=1, label="Trial Boundary" if idx == spatial_error_indices[0] else "")
+
+plt.xlabel("Targets")
+plt.ylabel("Spatial Error")
+plt.title("Spatial Errors Across Trials")
+plt.legend()
 plt.show()
