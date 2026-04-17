13d3e21dc6761d9b21758e6f0d18ab6ee5539439
Stylianos <stylgrig@yahoo.gr>
2026-04-17 18:33:21 +0300

    plots

---
+++ User_Figures.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reproducibility
+# np.random.seed(42)
+
+
+
+def fgn_sim(n=1000, H=0.7):
+    """Create Fractional Gaussian Noise
+     Inputs:
+            n: Number of data points of the time series. Default is 1000 data points.
+            H: Hurst parameter of the time series. Default is 0.7.
+     Outputs:
+            An array of n data points with variability H
+    # =============================================================================
+                                ------ EXAMPLE ------
+
+          - Create time series of 1000 datapoints to have an H of 0.7
+          n = 1000
+          H = 0.7
+          dat = fgn_sim(n, H)
+
+          - If you would like to plot the timeseries:
+          import matplotlib.pyplot as plt
+          plt.plot(dat)
+          plt.title(f"Fractional Gaussian Noise (H = {H})")
+          plt.xlabel("Time")
+          plt.ylabel("Value")
+          plt.show()
+    # =============================================================================
+    """
+
+    # Settings:
+    mean = 0
+    std = 1
+
+    # Generate Sequence:
+    z = np.random.normal(size=2 * n)
+    zr = z[:n]
+    zi = z[n:]
+    zic = -zi
+    zi[0] = 0
+    zr[0] = zr[0] * np.sqrt(2)
+    zi[n - 1] = 0
+    zr[n - 1] = zr[n - 1] * np.sqrt(2)
+    zr = np.concatenate([zr[:n], zr[n - 2::-1]])
+    zi = np.concatenate([zi[:n], zic[n - 2::-1]])
+    z = zr + 1j * zi
+
+    k = np.arange(n)
+    gammak = (np.abs(k - 1) ** (2 * H) - 2 * np.abs(k) ** (2 * H) + np.abs(k + 1) ** (2 * H)) / 2
+    ind = np.concatenate([np.arange(n - 1), [n - 1], np.arange(n - 2, 0, -1)])
+    gammak = gammak[ind]  # Circular shift of gammak to match n
+    gkFGN0 = np.fft.ifft(gammak)
+    gksqrt = np.real(gkFGN0)
+
+    if np.all(gksqrt > 0):
+        gksqrt = np.sqrt(gksqrt)
+        z = z[:len(gksqrt)] * gksqrt
+        z = np.fft.ifft(z)
+        z = 0.5 * (n - 1) ** (-0.5) * z
+        z = np.real(z[:n])
+    else:
+        gksqrt = np.zeros_like(gksqrt)
+        raise ValueError("Re(gk)-vector not positive")
+
+    # Standardize: (z - np.mean(z)) / np.sqrt(np.var(z))
+    ans = std * z + mean
+    return ans
+
+# Data
+# n_points = 50
+# x = np.arange(n_points)
+# y = np.random.uniform(0, 10, n_points)
+#
+# # Create figure
+# fig, ax = plt.subplots(figsize=(8, 4))
+#
+# # Scatter only
+# ax.scatter(x, y, s=90, c='red')
+#
+# # Remove EVERYTHING else
+# ax.axis('off')
+#
+# # Make background transparent
+# fig.patch.set_alpha(0)
+# ax.set_facecolor('none')
+#
+# # Save as transparent PNG
+# # plt.savefig("random_scatter.png", transparent=True, bbox_inches='tight', pad_inches=0)
+#
+# plt.show()
+#
+
+
+# Data
+n_points = 50
+x = np.arange(n_points)
+y = fgn_sim(n=n_points, H=0.99)
+
+# Create figure
+fig, ax = plt.subplots(figsize=(8, 4))
+
+# Scatter only
+ax.plot(x, y, c='red', alpha=0.5)
+ax.scatter(x, y, s=50, c='red')
+
+# Remove EVERYTHING else
+ax.axis('off')
+
+# Make background transparent
+fig.patch.set_alpha(0)
+ax.set_facecolor('none')
+
+# Save as transparent PNG
+# plt.savefig("random_scatter.png", transparent=True, bbox_inches='tight', pad_inches=0)
+
+plt.show()
